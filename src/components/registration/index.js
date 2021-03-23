022cebfaa7bf67561418d93695dd522af33b9683
John Doe <kurbanof.atabek@gmail.com>
2021-03-23 18:50:57 +0600

    removed no - need props

--- src/components/registration/index.js
+++ src/components/registration/index.js
@@ -16,7 +16,7 @@ const Registration = () => {
 		<RegisterBody>
 			<RegisterForm>
 				<RegisterTitle>Регистрация</RegisterTitle>
-				<Input setValue={setRegisterBody} registerBody={registerBody}  value={registerBody.name} text="Введите имя..." name="name"/>
+				<Input setValue={setRegisterBody} value={registerBody.name} text="Введите имя..." name="name"/>
 				<Input setValue={setRegisterBody} value={registerBody.email} text="Введите адрес электронной почты..." name="email"/>
 				<Input setValue={setRegisterBody} value={registerBody.password} text="Введите пароль..." type='password' name="password"/>
 				<AuthButton text="Войти"/>
